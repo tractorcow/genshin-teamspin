011b1f7d268c26498a7a7b93fa3d20540d7535ff
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2023-09-13 23:52:12 +1200

    feat: built out basic team customiser

--- src/components/TeamGenerator.tsx
+++ src/components/TeamGenerator.tsx
@@ -1,9 +1,9 @@
 import CharacterIcon from './CharacterIcon'
 import BlankIcon from './BlankIcon'
-import React from 'react'
+import React, { useState } from 'react'
 import { Character } from 'genshin-db'
 import { filterNamed } from '../lib/queries'
-import { selectRandomFour } from '../lib/selector'
+import { getTeamType, selectRandomFour, teamTypes } from '../lib/selector'
 
 interface TeamGeneratorProps {
   characters: Array<Character>
@@ -22,8 +22,14 @@ const TeamGenerator = ({
   const generateTeam = () => {
     setTeam(selectRandomFour(characters))
   }
+  const [showOptions, setShowOptions] = useState(false)
+  const [teamType, setTeamType] = useState('')
+  const [selection, setSelection] = useState('all')
+  const [includeHealer, setIncludeHealer] = useState(false)
+  const selectedType = getTeamType(teamType)
+
   return (
-    <div>
+    <div className="mb-12">
       <div className="grid grid-cols-4 gap-4 mb-4">
         {selectedCharacters.map((character) => (
           <CharacterIcon key={`char_${character.name}`} character={character} />
@@ -35,19 +41,106 @@ const TeamGenerator = ({
 
       <div className="mb-4 flex flex-row gap-6">
         <button
-          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
           onClick={generateTeam}
         >
           Generate {name}
         </button>
 
         <button
-          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
           onClick={() => setTeam([])}
         >
           Clear {name}
         </button>
+
+        <button
+          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setShowOptions((shown) => !shown)}
+        >
+          {showOptions ? 'Hide options' : 'Show extra options'}
+        </button>
       </div>
+
+      {showOptions && (
+        <div className="p-4 border rounded border-black w-full max-w-md mx-auto">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Team Type
+            </label>
+            <select
+              className="w-full p-2"
+              value={teamType}
+              onChange={(e) => setTeamType(e.target.value)}
+            >
+              <option value="">Select team type</option>
+              {teamTypes.map((group) => (
+                <optgroup key={group.group} label={group.group}>
+                  {group.types.map((type) => (
+                    <option key={type.name} value={type.name}>
+                      {type.name}
+                    </option>
+                  ))}
+                </optgroup>
+              ))}
+            </select>
+            {selectedType && (
+              <p className="italic text-sm p-3">{selectedType.description}</p>
+            )}
+          </div>
+
+          <div className="mb-4">
+            <span className="text-gray-700 text-sm font-bold">Genders</span>
+            <div className="mt-2">
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  className="form-radio"
+                  name="selection"
+                  value="all"
+                  checked={selection === 'all'}
+                  onChange={(e) => setSelection(e.target.value)}
+                />
+                <span className="ml-2">All</span>
+              </label>
+              <label className="inline-flex items-center ml-6">
+                <input
+                  type="radio"
+                  className="form-radio"
+                  name="selection"
+                  value="male"
+                  checked={selection === 'male'}
+                  onChange={(e) => setSelection(e.target.value)}
+                />
+                <span className="ml-2">Male Only</span>
+              </label>
+              <label className="inline-flex items-center ml-6">
+                <input
+                  type="radio"
+                  className="form-radio"
+                  name="selection"
+                  value="female"
+                  checked={selection === 'female'}
+                  onChange={(e) => setSelection(e.target.value)}
+                />
+                <span className="ml-2">Female Only</span>
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                className="form-checkbox"
+                checked={includeHealer}
+                onChange={() => setIncludeHealer(!includeHealer)}
+              />
+              <span className="ml-2">Include at least one healer</span>
+            </label>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/lib/selector.ts
+++ src/lib/selector.ts
@@ -1,6 +1,490 @@
 import type { Character } from 'genshin-db'
 import { getNames } from './queries'
 
+export enum Element {
+  Cryo = 'Cryo',
+  Pyro = 'Pyro',
+  Hydro = 'Hydro',
+  Electro = 'Electro',
+  Geo = 'Geo',
+  Anemo = 'Anemo',
+  Dendro = 'Dendro',
+}
+
+export enum WeaponType {
+  Claymore = 'Claymore',
+  Sword = 'Sword',
+  Bow = 'Bow',
+  Catalyst = 'Catalyst',
+  Polearm = 'Polearm',
+}
+
+export enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+}
+
+// Restriction rules for a team member. Can be empty if slot is open to all.
+type TeamMemberType = {
+  // For specific named character
+  name?: string
+  // 'same' means same as any other character, different means different to all other characters
+  element?: Element | 'same' | 'different' | Array<Element>
+  // for specific weapon type
+  weapontype?: WeaponType
+  // gender
+  gender?: Gender
+}
+
+type TeamType = {
+  name: string
+  description: string
+  // Note: Characters are matched left to right, so use more restrictive
+  // rules first, with 'different' element rules at the end
+  members: [TeamMemberType, TeamMemberType, TeamMemberType, TeamMemberType]
+}
+
+// Simple grouping of types
+type TypeGroup = {
+  group: string
+  types: Array<TeamType>
+}
+
+export const teamTypes: Array<TypeGroup> = [
+  {
+    group: 'Basic Reactions',
+    types: [
+      {
+        name: 'Quicken',
+        description: 'Dendro and Electro',
+        members: [
+          { element: Element.Dendro },
+          { element: Element.Electro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Electro,
+              Element.Anemo,
+              Element.Geo,
+            ],
+          },
+          {
+            element: [
+              Element.Dendro,
+              Element.Electro,
+              Element.Anemo,
+              Element.Geo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Swirl',
+        description: 'A team that can create swirl with anemo',
+        members: [
+          { element: Element.Anemo },
+          {
+            element: [
+              Element.Cryo,
+              Element.Hydro,
+              Element.Electro,
+              Element.Pyro,
+            ],
+          },
+          {
+            element: [
+              Element.Cryo,
+              Element.Hydro,
+              Element.Electro,
+              Element.Pyro,
+            ],
+          },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Crystalize',
+        description: 'A team that can make shields',
+        members: [
+          { element: Element.Geo },
+          {
+            element: [
+              Element.Cryo,
+              Element.Hydro,
+              Element.Electro,
+              Element.Pyro,
+            ],
+          },
+          {
+            element: [
+              Element.Cryo,
+              Element.Hydro,
+              Element.Electro,
+              Element.Pyro,
+            ],
+          },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Electro-Charged',
+        description: 'Hydro and electro (electro DoT)',
+        members: [
+          { element: Element.Electro },
+          { element: Element.Hydro },
+          {
+            element: [
+              Element.Hydro,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+          {
+            element: [
+              Element.Hydro,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Overloaded',
+        description: 'Pyro and electro (AoE Pyro DoT)',
+        members: [
+          { element: Element.Electro },
+          { element: Element.Pyro },
+          {
+            element: [
+              Element.Pyro,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+          {
+            element: [
+              Element.Pyro,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Freeze',
+        description: 'Cryo and hydro to immobilise',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Hydro },
+          {
+            element: [Element.Cryo, Element.Hydro, Element.Geo, Element.Anemo],
+          },
+          {
+            element: [Element.Cryo, Element.Hydro, Element.Geo, Element.Anemo],
+          },
+        ],
+      },
+      {
+        name: 'Melt',
+        description: 'Pyro and cryo',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Pyro },
+          { element: [Element.Cryo, Element.Pyro, Element.Geo, Element.Anemo] },
+          { element: [Element.Cryo, Element.Pyro, Element.Geo, Element.Anemo] },
+        ],
+      },
+      {
+        name: 'Superconduct',
+        description: 'Cryo and Electro',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Electro },
+          {
+            element: [
+              Element.Cryo,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+          {
+            element: [
+              Element.Cryo,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Vaporise',
+        description: 'Pyro and hydro',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Hydro },
+          { element: [Element.Cryo, Element.Pyro, Element.Geo, Element.Anemo] },
+          { element: [Element.Cryo, Element.Pyro, Element.Geo, Element.Anemo] },
+        ],
+      },
+      {
+        name: 'Burning',
+        description: 'Pyro and dendro',
+        members: [
+          { element: Element.Pyro },
+          { element: Element.Dendro },
+          {
+            element: [Element.Dendro, Element.Pyro, Element.Geo, Element.Anemo],
+          },
+          {
+            element: [Element.Dendro, Element.Pyro, Element.Geo, Element.Anemo],
+          },
+        ],
+      },
+      {
+        name: 'Bloom',
+        description: 'Hydro and dendro',
+        members: [
+          { element: Element.Hydro },
+          { element: Element.Dendro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Hydro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+          {
+            element: [
+              Element.Dendro,
+              Element.Hydro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    group: 'Advanced Reactions',
+    types: [
+      {
+        name: 'Super-Freeze',
+        description: 'Hydro and double cryo for extra crit',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Cryo },
+          { element: Element.Hydro },
+          {
+            element: [Element.Cryo, Element.Hydro, Element.Geo, Element.Anemo],
+          },
+        ],
+      },
+      {
+        name: 'Hyperbloom',
+        description: 'Hydro, dendro, and electro',
+        members: [
+          { element: Element.Hydro },
+          { element: Element.Dendro },
+          { element: Element.Electro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Hydro,
+              Element.Electro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Burgeon',
+        description: 'Hydro, dendro, and pyro',
+        members: [
+          { element: Element.Hydro },
+          { element: Element.Dendro },
+          { element: Element.Pyro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Hydro,
+              Element.Pyro,
+              Element.Geo,
+              Element.Anemo,
+            ],
+          },
+        ],
+      },
+
+      {
+        name: 'Aggravate',
+        description: 'Dendro and two electro for aggravate damage',
+        members: [
+          { element: Element.Dendro },
+          { element: Element.Electro },
+          { element: Element.Electro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Electro,
+              Element.Anemo,
+              Element.Geo,
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Spread',
+        description: 'Electro with two dendro for spread damage',
+        members: [
+          { element: Element.Dendro },
+          { element: Element.Dendro },
+          { element: Element.Electro },
+          {
+            element: [
+              Element.Dendro,
+              Element.Electro,
+              Element.Anemo,
+              Element.Geo,
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    group: 'Elemental Resonance',
+    types: [
+      {
+        name: 'Quad-Elemental',
+        description: 'Four different elements for resistance bonus',
+        members: [
+          {},
+          { element: 'different' },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Geo',
+        description: 'Two geo for geo damage bonus',
+        members: [
+          { element: Element.Geo },
+          { element: Element.Geo },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Pyro',
+        description: 'Two pyro for attack bonus',
+        members: [
+          { element: Element.Pyro },
+          { element: Element.Pyro },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Cryo',
+        description: 'Two cryo for crit rate bonus',
+        members: [
+          { element: Element.Cryo },
+          { element: Element.Cryo },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Hydro',
+        description: 'Two hydro for HP bonus',
+        members: [
+          { element: Element.Hydro },
+          { element: Element.Hydro },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Anemo',
+        description: 'Two anemo for movement bonus',
+        members: [
+          { element: Element.Anemo },
+          { element: Element.Anemo },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Dendro',
+        description: 'Two dendro for reaction elemental bonus',
+        members: [
+          { element: Element.Dendro },
+          { element: Element.Dendro },
+          {
+            // Dendro resonance needs one of these to actually have effect
+            element: [Element.Hydro, Element.Pyro, Element.Electro],
+          },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Double Elecro',
+        description: 'Two electro, two non-electro',
+        members: [
+          { element: Element.Electro },
+          { element: Element.Electro },
+          { element: 'different' },
+          { element: 'different' },
+        ],
+      },
+    ],
+  },
+  {
+    group: 'Specialist Teams',
+    types: [
+      {
+        name: 'Mono-Elemental',
+        description: 'All characters of the same element',
+        members: [
+          {},
+          { element: 'same' },
+          { element: 'same' },
+          { element: 'same' },
+        ],
+      },
+      {
+        name: 'Gorou Triple-Geo',
+        description: 'Miss Hina and her entourage',
+        members: [
+          { name: 'Gorou' },
+          { element: Element.Geo },
+          { element: Element.Geo },
+          { element: 'different' },
+        ],
+      },
+      {
+        name: 'Nilou Bountiful Bloom',
+        description: 'Nilou and hydro / dendro only for her special reaction',
+        members: [
+          { name: 'Nilou' },
+          { element: Element.Dendro },
+          { element: [Element.Dendro, Element.Hydro] },
+          { element: [Element.Dendro, Element.Hydro] },
+        ],
+      },
+    ],
+  },
+]
+
 /**
  * Get random team from this list
  * @param characters
@@ -21,4 +505,23 @@ export function selectRandomFour(characters: Array<Character>): string[] {
     names.splice(randomIndex, 1) // Remove the selected name to avoid duplicates
   }
   return selectedNames
+}
+
+/**
+ * Get a team type config by name
+ *
+ * @param type
+ */
+export function getTeamType(type: string | undefined): TeamType | undefined {
+  if (!type) {
+    return undefined
+  }
+  for (const group of teamTypes) {
+    for (const nextType of group.types) {
+      if (nextType.name === type) {
+        return nextType
+      }
+    }
+  }
+  return undefined
 }
